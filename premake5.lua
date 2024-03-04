daa61806a5aec191aad9d5f054c735059e27dbcb
Adam <87928802+DnA-IntRicate@users.noreply.github.com>
2024-03-04 19:54:26 +0200

    Update submodules

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,5 @@
 include "Vendor/premake/customization/solutionitems.lua"
 
-require "Vendor/premake/premake-cmake/cmake"
-
 OUT_DIR = "%{wks.location}/bin/build/%{cfg.system}/%{cfg.architecture}/%{cfg.buildcfg}"
 INT_DIR = "%{wks.location}/bin/intermediate/%{cfg.system}/%{cfg.architecture}/%{cfg.buildcfg}"
 
